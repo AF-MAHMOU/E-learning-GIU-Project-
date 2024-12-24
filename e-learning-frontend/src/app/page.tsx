6a3d01ef7a35a5c4a28029a2568e208289ccd997
AF-MAHMOU <ahmed.mahmoud03092004@gmail.com>
2024-12-25 00:15:55 +0200

    Add backend as regular directory

--- e-learning-frontend/src/app/page.tsx
+++ e-learning-frontend/src/app/page.tsx
@@ -19,7 +19,7 @@ export default function HomePage() {
         </p>
 
         <div className="flex space-x-4">
-          {/* Show Courses button */}
+          {/* Show Courses button */}   
           <Link
             href="/courses"
             className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition"
